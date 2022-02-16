9366e5926473f8087ed77f705cb297a105ffb021
rajat <rajat2411gupta@gmail.com>
2022-02-16 22:27:22 +0530

    did nothing, just some DSA on leetcode

---
+++ recursion/firstCaptitalLetter/code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if (n == 1) return 1;
+        
+        else if (n == 0) return 0;
+        
+        return fib(n-1) + fib(n-2);
+    }
+};
